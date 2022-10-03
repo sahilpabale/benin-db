6b330e9db6d60c554aecf19c2dd72ae8a98899b6
rishit khandelwal <rishitkh@gmail.com>
2022-10-03 21:36:54 +0530

    Add cli input from client

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,9 +5,15 @@ use tokio::{io::AsyncWriteExt, net::TcpStream};
 #[tokio::main]
 async fn main() {
     let mut stream = TcpStream::connect("127.0.0.1:6379").await.unwrap();
+    let command = cmd_from_arg();
+    stream.write_all(command.as_bytes()).await.unwrap();
+}
 
-    stream
-        .write_all(&"set name benin-db".as_bytes())
-        .await
-        .unwrap();
+fn cmd_from_arg() -> String {
+    let args: Vec<String> = env::args().collect();
+
+    let args: Vec<String> = args[1..].to_vec();
+
+    let command = args.join(" ");
+    command
 }
