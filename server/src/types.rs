304c601068102073d022d9705682a5a7c712f6f5
Kardev07 <77888951+Kardev07@users.noreply.github.com>
2022-10-16 19:44:30 +0530

    chore: add documentation to server/src/main.rs && types.rs

--- server/src/types.rs
+++ server/src/types.rs
@@ -1,5 +1,7 @@
 #[derive(Debug)]
 pub enum Command {
+    // getting values using a `key` as input
     Get { key: String },
+    // setting values using a `key` and `val` as input
     Set { key: String, val: String },
 }
