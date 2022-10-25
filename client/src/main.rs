d3728d7d37356a0bc2905b065b464700dc497084
Kardev07 <77888951+Kardev07@users.noreply.github.com>
2022-10-25 23:36:06 +0530

    feat: add documentation for client/src/main.rs

--- client/src/main.rs
+++ client/src/main.rs
@@ -4,16 +4,22 @@ use tokio::{io::AsyncWriteExt, net::TcpStream};
 
 #[tokio::main]
 async fn main() {
+    // creating a tcp stream connected to port `127.0.0.1` on port `6379`
     let mut stream = TcpStream::connect("127.0.0.1:6379").await.unwrap();
+    // initializing the `cmd_from_arg` to a `command` variable 
     let command = cmd_from_arg();
+    // writing the command to the tcp stream
     stream.write_all(command.as_bytes()).await.unwrap();
 }
 
+// function returning a `String` type
 fn cmd_from_arg() -> String {
+    // getting the arguments passed in the request in the form of a `Vec<String>` type
     let args: Vec<String> = env::args().collect();
 
     let args: Vec<String> = args[1..].to_vec();
 
+    // joining the arguments separated by " "
     let command = args.join(" ");
     command
 }
